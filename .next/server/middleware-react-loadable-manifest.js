6b8af3a9f00dd9795ea277eafc114b08164739f3
Kawish Iqbal <kawishiqbal898@gmail.com>
2026-01-15 22:03:34 +0500

    clean up .next build files

--- .next/server/middleware-react-loadable-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__REACT_LOADABLE_MANIFEST="{\"_app.js -> ../components/CartSidebar\":{\"id\":\"_app.js -> ../components/CartSidebar\",\"files\":[\"static/chunks/components_CartSidebar_js.js\"]}}"
